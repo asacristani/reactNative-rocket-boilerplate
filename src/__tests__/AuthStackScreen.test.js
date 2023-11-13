d7d583b407a482718117e6ce0cdad2f9660fe120
AdrianPayne <adriansacristan1993@gmail.com>
2023-11-13 04:31:12 +0000

    Fix style 2

--- src/__tests__/AuthStackScreen.test.js
+++ src/__tests__/AuthStackScreen.test.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { AuthStackScreen } from "../navigation";
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {AuthStackScreen} from '../navigation';
 
-describe("AuthStackScreen", () => {
-  it("renders correctly", () => {
-    const { getByText } = render(
-      <NavigationContainer>
-        <AuthStackScreen />
-      </NavigationContainer>,
+describe('AuthStackScreen', () => {
+  it('renders correctly', () => {
+    const {getByText} = render(
+        <NavigationContainer>
+          <AuthStackScreen />
+        </NavigationContainer>,
     );
 
-    expect(getByText("Home")).toBeTruthy();
-    expect(getByText("Me")).toBeTruthy();
+    expect(getByText('Home')).toBeTruthy();
+    expect(getByText('Me')).toBeTruthy();
   });
 });
--- src/screens/OnboardingScreen/OnboardingScreen.js
+++ src/screens/OnboardingScreen/OnboardingScreen.js
@@ -1,9 +1,9 @@
-import { Image, Text, View } from "react-native";
-import React, { useState } from "react";
-import { Button } from "react-native-paper";
-import theme from "../../style/theme";
+import {Image, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button} from 'react-native-paper';
+import theme from '../../style/theme';
 
-export default function OnboardingScreen({ navigation }) {
+export default function OnboardingScreen({navigation}) {
   const [currentPage, setCurrentPage] = useState(1);
 
   const handleNext = () => {
@@ -13,33 +13,33 @@ export default function OnboardingScreen({ navigation }) {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", padding: 20 }}>
-      <View style={{ flex: 1, alignItems: "center", padding: 20 }}>
+    <View style={{flex: 1, alignItems: 'center', padding: 20}}>
+      <View style={{flex: 1, alignItems: 'center', padding: 20}}>
         <Image
-          source={require("../../../assets/icon.png")}
-          style={{ height: 200, marginBottom: 20 }}
+          source={require('../../../assets/icon.png')}
+          style={{height: 200, marginBottom: 20}}
           resizeMode="contain"
         />
-        <Text style={{ fontSize: 24, fontWeight: "bold", marginTop: 20 }}>
-          Welcome to{"\n"}
+        <Text style={{fontSize: 24, fontWeight: 'bold', marginTop: 20}}>
+          Welcome to{'\n'}
           ReactNative-Rocket-Boilerplate
         </Text>
-        <Text style={{ fontSize: 16, textAlign: "center", marginVertical: 20 }}>
+        <Text style={{fontSize: 16, textAlign: 'center', marginVertical: 20}}>
           This is the onboarding of your mobile app
         </Text>
       </View>
       <View
         style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: "100%",
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '100%',
         }}
       >
         <Button
           mode="text"
-          onPress={() => navigation.navigate("Login")}
+          onPress={() => navigation.navigate('Login')}
           textColor={theme.colors.primary}
-          style={{ flex: 1 }}
+          style={{flex: 1}}
         >
           Skip
         </Button>
